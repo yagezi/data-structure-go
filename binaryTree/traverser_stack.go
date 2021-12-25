582b18ed03ae66d8a71c01a139dbd3470f250139
Karl Lu <lkcnju@126.com>
2021-12-25 19:48:51 +0800

    Create traverser_stack.go

---
+++ binaryTree/traverser_stack.go
@@ -0,0 +1,86 @@
+package binarytree
+
+import (
+	"fmt"
+
+	"github.com/yagezi/data-structure-go/queue"
+	"github.com/yagezi/data-structure-go/stack"
+)
+
+func LevelOrderQueue(tree *BiTree) {
+	if tree.head == nil {
+		return
+	}
+	str := ""
+	q := queue.NewQueue()
+	q.Enqueue(tree.head)
+	for !q.IsEmpty() {
+		n := q.Dequeue().(*BiTNode)
+		str += fmt.Sprintf("%s ", string(n.key.(byte)))
+		if n.left != nil {
+			q.Enqueue(n.left)
+		}
+		if n.right != nil {
+			q.Enqueue(n.right)
+		}
+	}
+	fmt.Println(str)
+}
+
+func PreOrderStack(tree *BiTree) {
+	str := ""
+	s := stack.NewStack(100)
+	cur := tree.head
+
+	for !s.IsEmpty() || cur != nil {
+		if cur != nil {
+			s.Push(cur)
+			str += fmt.Sprintf("%s ", string(cur.key.(byte)))
+			cur = cur.left
+		} else {
+			n, _ := s.Pop()
+			cur = n.(*BiTNode).right
+		}
+	}
+	fmt.Println(str)
+}
+
+func InOrderStack(tree *BiTree) {
+	str := ""
+	s := stack.NewStack(100)
+	cur := tree.head
+
+	for !s.IsEmpty() || cur != nil {
+		if cur != nil {
+			s.Push(cur)
+			cur = cur.left
+		} else {
+			n, _ := s.Pop()
+			cur = n.(*BiTNode)
+			str += fmt.Sprintf("%s ", string(cur.key.(byte)))
+			cur = cur.right
+		}
+	}
+	fmt.Println(str)
+}
+
+func PostOrderStack(tree *BiTree) {
+	str := ""
+	s := stack.NewStack(100)
+	flags := stack.NewStack(100)
+	cur := tree.head
+
+	for !s.IsEmpty() || cur != nil {
+		if cur != nil {
+			s.Push(cur)
+			flags.Push(cur)
+			cur = cur.left
+		} else {
+			n, _ := s.Pop()
+			cur = n.(*BiTNode)
+			str += fmt.Sprintf("%s ", string(cur.key.(byte)))
+			cur = cur.right
+		}
+	}
+	fmt.Println(str)
+}
