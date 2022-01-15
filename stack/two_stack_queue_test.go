c64b153f4e6f6dc92952dc8598f461ec77b767a2
Karl Lu <lkcnju@126.com>
2022-01-15 15:54:07 +0800

    Update two_stack_queue_test.go

--- stack/two_stack_queue_test.go
+++ stack/two_stack_queue_test.go
@@ -1,30 +1,38 @@
 package stack
 
-type Queue struct {
-	in  *Stack
-	out *Stack
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestTwoStackQueue(t *testing.T) {
+	Convey("given an empty queue", t, func() {
+		queue := NewQueue()
+
+		Convey("when pop", func() {
+			e := queue.Pop()
+
+			Convey("then should be empty", func() {
+				So(e, ShouldBeNil)
+			})
+		})
+	})
 }
 
-func NewQueue() *Queue {
-	return &Queue{
-		in:  NewStack(),
-		out: NewStack(),
-	}
-}
-
-func (q *Queue) Push(e Element) {
-	q.in.Push(e)
-}
-
-func (q *Queue) Pop() Element {
-	if q.out.IsEmpty() {
-		q.roll()
-	}
-	return q.out.Pop()
-}
-
-func (q *Queue) roll() {
-	for !q.in.IsEmpty() {
-		q.out.Push(q.in.Pop())
-	}
+func TestQueue(t *testing.T) {
+	queue := NewQueue()
+	queue.Push("1")
+	queue.Push("2")
+	queue.Push("3")
+	queue.Push("4")
+	fmt.Println(queue.Pop())
+	fmt.Println(queue.Pop())
+	fmt.Println(queue.Pop())
+	fmt.Println(queue.Pop())
+	queue.Push("1")
+	queue.Push("2")
+	fmt.Println(queue.Pop())
+	fmt.Println(queue.Pop())
 }
