079bd2ad8740fee5d2758187c284233aa66bba8e
Karl Lu <lkcnju@126.com>
2022-01-15 15:30:27 +0800

    Create cmp.go

---
+++ heap/cmp.go
@@ -0,0 +1,22 @@
+/*
+*@Time        : 2022/01/11
+*@File        : cmp.go
+*@Description :
+ */
+
+package heap
+
+import (
+	"strings"
+)
+
+type CmpFunc func(a, b interface{}) int
+
+func CmpString(a, b interface{}) int {
+	return strings.Compare(a.(string), b.(string))
+}
+
+func CmpInt(a, b interface{}) int {
+	return a.(int) - b.(int)
+}
+
