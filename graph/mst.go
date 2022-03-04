ca864b2455de090492caf27a90896c9dc22d488f
Karl Lu <lkcnju@126.com>
2022-03-04 19:43:52 +0800

    Update mst.go

--- graph/mst.go
+++ graph/mst.go
@@ -7,15 +7,16 @@
 package graph
 
 import (
+	"container/heap"
 	"fmt"
 	"sort"
 
 	"github.com/yagezi/data-structure-go/dsu"
 )
 
-// TODO: prim and kruskal
-
-func mst_kruskal(g *Graph) *Graph {
+// tips: using disjoint set union
+func mst_kruskal(g *Graph) {
+	weightSum := 0
 	dsu := dsu.NewDSU(g.e)
 	edges := make([]*Edge, 0, g.e)
 	for _, es := range g.adj {
@@ -27,17 +28,14 @@ func mst_kruskal(g *Graph) *Graph {
 		return edges[i].w < edges[j].w
 	})
 	// dbgPrintEdges(edges)
-	res := NewGraph(g.v)
 	for _, e := range edges {
 		if !dsu.In(e.sid, e.tid) {
 			dsu.Union(e.sid, e.tid)
-			res.addEdge(e.sid, e.tid, e.w)
-			if res.e == res.v-1 {
-				break
-			}
+			weightSum += e.w
+			fmt.Printf("src: %d, dst %d, weight: %d\n", e.sid, e.tid, e.w)
 		}
 	}
-	return res
+	fmt.Println("sum of weight:", weightSum)
 }
 
 func dbgPrintEdges(edges []*Edge) {
@@ -46,3 +44,29 @@ func dbgPrintEdges(edges []*Edge) {
 		fmt.Println(v)
 	}
 }
+
+// tips: using priority queue or min-heap
+func mst_prim(g *Graph) {
+	h := &VertexHeap{}
+	heap.Init(h)
+	mstSet := make([]bool, g.v)
+	weightSum := 0
+	// Vertex.value means distance to mst
+	heap.Push(h, &Vertex{id: 0, value: 0})
+	for h.Len() > 0 {
+		v := heap.Pop(h).(*Vertex)
+		if !mstSet[v.id] {
+			mstSet[v.id] = true
+			weightSum += v.value.(int)
+			fmt.Printf("node id: %d, weight: %d\n", v.id, v.value)
+			for e := g.adj[v.id].Front(); e != nil; e = e.Next() {
+				edge := e.Value.(*Edge)
+				if !mstSet[edge.tid] {
+					heap.Push(h, &Vertex{id: edge.tid, value: edge.w})
+				}
+			}
+		}
+	}
+	fmt.Println("sum of weight:", weightSum)
+}
+
